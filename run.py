d5d4af2b5c89c352676eef971098f34001817a70
jakejamesreid <jakejamesreid@gmail.com>
2020-05-26 11:53:54 +0100

    Added flash message to contact form

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
-
+app.secret_key = "some_secret"
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -15,6 +15,7 @@ def about():
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
 
+
 @app.route("/about/<member_name>")
 def about_member(member_name):
     member = {}
@@ -25,9 +26,14 @@ def about_member(member_name):
                 member = obj
     return render_template("member.html", member=member)
 
-@app.route("/contact")
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks "+request.form['name']+", we have received your message")
+        print(request.form["name"])
     return render_template("contact.html", page_title="Contact")
+
 
 @app.route("/careers")
 def careers():
