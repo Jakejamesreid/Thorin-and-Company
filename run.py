1fdc642d9db8eb6c92b94ff2faa5936984b64ede
jakejamesreid <jakejamesreid@gmail.com>
2020-06-11 16:49:40 +0100

    Added small fix

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 import os
 import json
+import requests
 from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
 app.secret_key = "some_secret"
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -15,13 +17,13 @@ def about():
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
 
-
 @app.route("/about/<member_name>")
 def about_member(member_name):
     member = {}
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
         for obj in data:
+            print(type(obj))
             if obj["url"] == member_name:
                 member = obj
     return render_template("member.html", member=member)
@@ -42,5 +44,5 @@ def careers():
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"), 
-    port=int(os.environ.get("PORT")),
+    port=os.environ.get("PORT"),
     debug=True)
